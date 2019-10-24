d306785165486db8d19d7143e06f10e29011d4b6
Nagireddy <nagireddy.panditi@gmail.com>
2019-10-24 11:24:44 +0530

    fouth commit

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 // const {mul,add,min,div} =require("./helpers")
 // const tot =mul(10,100);
-// const to =add(100,100);
 // const t =min(15,5);
 // const o =div(15,5);
 // console.log(tot);
-// console.log(to);
 // console.log(t);
 // console.log(o);
 // const http= require("http");
